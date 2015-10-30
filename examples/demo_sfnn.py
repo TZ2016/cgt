f2e2f434a031c693e1d37ac4eb1791064f6a6a49
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2015-10-30 15:07:24 -0700

    also supports batch of >= 1

--- examples/demo_sfnn.py
+++ examples/demo_sfnn.py
@@ -43,12 +43,10 @@ def main():
     parser.add_argument("--num_sto", type=int, nargs='+')
     args = parser.parse_args()
 
-    # TODO_TZ: not support multiple stochastic nodes in one layer
-
     sfnn = hybrid_network(args.num_inputs, args.num_units, args.num_sto)
     f_step, f_loss, f_grad = make_funcs(sfnn, args.num_inputs)
     for _ in range(10):
-        print f_step(np.array([[7]]))
+        print f_step(np.array([[7], [8]]))
 
 
 if __name__ == "__main__":
