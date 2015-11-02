0b1cfdea73ffc42a9083ae9c50270df48f11acf5
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2015-11-01 21:02:17 -0800

    some comments on usage of SFNN

--- examples/demo_sfnn.py
+++ examples/demo_sfnn.py
@@ -43,10 +43,18 @@ def main():
     parser.add_argument("--num_sto", type=int, nargs='+')
     args = parser.parse_args()
 
+    # Hybrid layer with multiple stochastic nodes
+    # And pure stochastic layer
+    # --num_inputs 1 --num_units 3 2 --num_sto 2 2
+
     sfnn = hybrid_network(args.num_inputs, args.num_units, args.num_sto)
     f_step, f_loss, f_grad = make_funcs(sfnn, args.num_inputs)
     for _ in range(10):
+        # The number does not matter without training
+        print f_step(np.array([[7]]))
+        # The following features batch_size > 1
         print f_step(np.array([[7], [8]]))
+        # Verify that the output is stochastic
 
 
 if __name__ == "__main__":
