98f73be889638fed55517943e05e098c333f24e5
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2015-11-02 16:43:38 -0800

    also output stochastic values

--- examples/demo_sfnn.py
+++ examples/demo_sfnn.py
@@ -5,38 +5,41 @@ from cgt import nn
 import numpy as np
 
 
-def hybrid_layer(size_in, size_out, size_random):
+def hybrid_layer(X, size_in, size_out, size_random):
     assert size_out >= size_random
-    X = cgt.matrix(fixed_shape=(None, size_in))
     out = cgt.sigmoid(nn.Affine(size_in, size_out)(X))
-    if size_random > 0:
-        out = cgt.concatenate([
-            cgt.bernoulli(out[:, :size_random]),
-            out[:, size_random:],
-        ], axis=1)
-    return nn.Module([X], [out])
+    if size_random == 0:
+        return out, None
+    else:
+        out_s = cgt.bernoulli(out[:, :size_random])
+        out = cgt.concatenate([out_s, out[:, size_random:]], axis=1)
+        return out, out_s
 
 
-def hybrid_network(size_in, num_units, num_sto):
+def hybrid_network(size_in, num_units, num_stos):
     X = cgt.matrix(fixed_shape=(None, size_in))
-    prev_num_units, prev_out = size_in, [X]
-    for (num_units, num_sto) in zip(num_units, num_sto):
-        layer = hybrid_layer(prev_num_units, num_units, num_sto)
-        prev_num_units, prev_out = num_units, layer(prev_out)
-    # TODO_TZ: missing output layer
-    return nn.Module([X], prev_out)
+    prev_num_units, prev_out = size_in, X
+    all_sto_out, net_out_sto = [], None
+    for (curr_num_units, curr_num_sto) in zip(num_units, num_stos):
+        prev_out, sto_out = hybrid_layer(
+            prev_out, prev_num_units, curr_num_units, curr_num_sto
+        )
+        if sto_out: all_sto_out.append(sto_out)
+        prev_num_units = curr_num_units
+    # TODO_TZ: missing output layer for the complete SFNN
+    net_out = prev_out
+    if all_sto_out: net_out_sto = cgt.concatenate(all_sto_out, axis=1)
+    return X, net_out, net_out_sto
 
 
-def make_funcs(network, size_in, size_out):
+def make_funcs(net_in, net_out, net_out_rand):
+    size_batch = net_in.shape[0]
     # step func
-    X = cgt.matrix("X", fixed_shape=(None, size_in))
-    Y_hat = network([X])[0]
-    f_step = cgt.function([X], [Y_hat])
+    f_step = cgt.function([net_in], [net_out, net_out_rand])
     # loss func
-    Y = cgt.matrix("Y", fixed_shape=(None, size_out))
-    size_batch = X.shape[0]
-    loss = cgt.sum(cgt.norm(Y_hat - Y, axis=1)) / size_batch
-    f_loss = cgt.function([X, Y], [Y_hat, loss])
+    Y = cgt.matrix()
+    loss = cgt.sum(cgt.norm(net_out - Y, axis=1)) / size_batch
+    f_loss = cgt.function([net_in, Y], [net_out, net_out_rand, loss])
     # grad func
     size_sample = 10  # number of samples for each example
     size_random = 10  # number of stochastic nodes
@@ -48,6 +51,16 @@ def make_funcs(network, size_in, size_out):
 
     # TODO_TZ: f_grad requires graph conversion
     return f_step, f_loss, None
+
+
+def nice_print(X, Y, func, kind="loss"):
+    assert X.shape[0] == Y.shape[0], "unequal batch size"
+    if kind == "loss":
+        net_out, net_out_rand, loss = func(X, Y)
+        print "====="
+        print net_out
+        print loss
+        print net_out_rand
 
 
 def main():
@@ -62,12 +75,12 @@ def main():
     # And pure stochastic layer
     # --num_inputs 1 --num_units 3 2 --num_sto 2 2
 
-    sfnn = hybrid_network(args.num_inputs, args.num_units, args.num_sto)
-    f_step, f_loss, f_grad = make_funcs(sfnn, args.num_inputs, args.num_units[-1])
+    X, out, out_rand = hybrid_network(args.num_inputs, args.num_units, args.num_sto)
+    f_step, f_loss, f_grad = make_funcs(X, out, out_rand)
     for _ in range(10):
         # The number does not matter without training
         # The following features batch_size > 1
-        print f_loss(np.array([[7], [8]]), np.array([[0, 0], [0, 0]]))
+        nice_print(np.array([[7], [8]]), np.array([[0, 0], [0, 0]]), f_loss)
         # Verify that the output is stochastic
 
 if __name__ == "__main__":
