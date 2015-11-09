4d7103c064e588b620ac99a7415dfcc5981cd4a0
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2015-11-09 13:57:23 -0800

    clean up demo_sfnn

--- examples/demo_sfnn.py
+++ examples/demo_sfnn.py
@@ -10,29 +10,15 @@ import traceback
 from scipy.special import expit as sigmoid
 from param_collection import ParamCollection
 from cgt.distributions import gaussian_diagonal
-from demo_char_rnn import make_rmsprop_state, rmsprop_update, Table
+from demo_char_rnn import make_rmsprop_state, Table
+
 
 def err_handler(type, flag):
-    print "OH NOOOO!"
     print type, flag
     traceback.print_exc()
     # raise FloatingPointError('refer to err_handler for more details')
-
 np.seterr(all='call')
 np.seterrcall(err_handler)
-
-DEFAULT_ARGS = Table(
-    # network
-    num_inputs=1,
-    num_outputs=1,
-    num_units=[4, 5, 5, 4],
-    num_sto=[0, 2, 2, 0],
-    no_bias=False,
-    # training
-    n_epochs=10,
-    step_size=.01,
-    decay_rate=.95,
-)
 
 
 def generate_examples(N, x, y, p_y):
@@ -87,7 +73,7 @@ def hybrid_network(size_in, size_out, num_units, num_stos):
     return X, net_out
 
 
-def make_funcs(net_in, net_out):
+def make_funcs(net_in, net_out, **kwargs):
     def f_grad (*x):
         out = f_surr(*x)
         return out['loss'], out['surr_loss'], out['surr_grad']
@@ -104,7 +90,7 @@ def make_funcs(net_in, net_out):
         Y, net_out[:, :size_out], out_sigma
     ) / size_batch
     params = nn.get_parameters(loss)
-    if DEFAULT_ARGS.no_bias:
+    if kwargs.has_key('no_bias'):
         params = [p for p in params if not p.name.endswith(".b")]
     f_loss = cgt.function([net_in, Y], [net_out, loss])
     # grad func
@@ -112,50 +98,70 @@ def make_funcs(net_in, net_out):
     return params, f_step, f_loss, f_grad, f_surr
 
 
-def nice_print(X, Y, func):
-    assert X.shape[0] == Y.shape[0], "unequal batch size"
-    _out = func(X, Y)
-    print "====="
-    if isinstance(_out, (list, tuple)):
-        for _o in _out:
-            print _o
-    elif isinstance(_out, dict):
-        for k, v in _out.iteritems():
-            print (k, v)
-    else:
-        print _out
-
-
-def main():
-    # import argparse
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--step_size", type=float, default=.01)
-    # parser.add_argument("--n_epochs", type=int, default=100)
-    # parser.add_argument("--decay_rate", type=float, default=.95)
-    # parser.add_argument("--num_inputs", type=int)
-    # parser.add_argument("--num_units", type=int, nargs='+')
-    # parser.add_argument("--num_sto", type=int, nargs='+')
-    # args = parser.parse_args()
-    args = DEFAULT_ARGS
-
-    # Hybrid layer with multiple stochastic nodes
-    # And pure stochastic layer
-    # --num_inputs 1 --num_units 3 2 --num_sto 2 2
-
-    net_in, net_out = hybrid_network(args.num_inputs, 2*args.num_outputs,
+def train(args, X, Y, dbg_epoch=None, dbg_iter=None):
+    net_in, net_out = hybrid_network(args.num_inputs, args.num_outputs,
                                      args.num_units, args.num_sto)
     params, f_step, f_loss, f_grad, f_surr = make_funcs(net_in, net_out)
     param_col = ParamCollection(params)
     param_col.set_value_flat(
         np.random.uniform(-.1, .1, size=(param_col.get_total_size(),))
     )
-    optim_state = make_rmsprop_state(theta=param_col.get_value_flat(),
-                                     step_size=args.step_size,
-                                     decay_rate=args.decay_rate)
+    optim_state = Table(theta=param_col.get_value_flat(),
+                        grad=param_col.get_value_flat(),
+                        step_size=args.step_size
+                        )
+    all_loss, all_surr_loss = [], []
+    for i_epoch in range(args.n_epochs):
+        for i_iter in range(X.shape[0]):
+            x, y = X[i_iter:i_iter+1], Y[i_iter:i_iter+1]
+            info = f_surr(x, y)
+            loss, loss_surr, grad = info['loss'], info['surr_loss'], info['surr_grad']
+            # loss, loss_surr, grad = f_grad(x, y)
+            all_loss.append(np.sum(loss))
+            all_surr_loss.append(loss_surr)
+            # update
+            optim_state.grad = param_col.flatten_values(grad)
+            optim_state.theta -= optim_state.step_size * optim_state.grad
+            param_col.set_value_flat(optim_state.theta)
+            if dbg_iter: dbg_iter(i_epoch, i_iter, optim_state, info)
+        if dbg_epoch: dbg_epoch(i_epoch, param_col, f_surr)
+    all_loss, all_surr_loss = np.array(all_loss), np.array(all_surr_loss)
+    plt.plot(np.convolve(all_loss, [1. / X.shape[0]] * X.shape[0], 'same'))
+    plt.plot(np.convolve(all_surr_loss, [1. / X.shape[0]] * X.shape[0], 'same'))
+
+
+if __name__ == "__main__":
+    #################3#####
+    #  for synthetic data #
+    #######################
+    args_synthetic = Table(
+        num_inputs=1,
+        num_outputs=2,
+        num_units=[4, 5, 5, 4],
+        num_sto=[0, 2, 2, 0],
+        no_bias=False,
+        n_epochs=10,
+        step_size=.01,
+    )
+    X_syn, Y_syn = data_synthetic_a(1000)
+    def syn_dbg_epoch(i_epoch, param_col, f_surr):
+        print "Epoch %d" % i_epoch
+        print "network parameters"
+        _params_val = param_col.get_values()
+        # _ber_param = _params_val[0].T.dot(EXAMPLES_ARGS.x)
+        # if not args.no_bias: _ber_param += _params_val[1].flatten()
+        # _ber_param = sigmoid(_ber_param)
+        # print ""
+        # print "network params"
+        # pprint.pprint(_params_val)
+        # print "bernoulli param"
+        # pprint.pprint( _ber_param)
+        pprint.pprint(_params_val)
+    def syn_dbg_iter(i_epoch, i_iter, optim_state, info):
+        print np.linalg.norm(optim_state.theta)
+    train(args_synthetic, X_syn, Y_syn, syn_dbg_epoch, syn_dbg_iter)
 
     # X, Y = generate_examples(10, np.array([3.]), np.array([0.]), [.1])
-    X, Y = data_synthetic_a(1000)
-
     # X1, Y1 = generate_examples(10,
     #                            np.array([3.]), np.array([0]),
     #                            [0.5])
@@ -167,38 +173,14 @@ def main():
     #                            [0.9])
     # X = np.concatenate([X1, X2, X3])
     # Y = np.concatenate([Y1, Y2, Y3])
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--step_size", type=float, default=.01)
+    # parser.add_argument("--n_epochs", type=int, default=100)
+    # parser.add_argument("--decay_rate", type=float, default=.95)
+    # parser.add_argument("--num_inputs", type=int)
+    # parser.add_argument("--num_units", type=int, nargs='+')
+    # parser.add_argument("--num_sto", type=int, nargs='+')
+    # args = parser.parse_args()
 
-    all_loss = []
-    all_surr_loss = []
-    for i_epoch in range(args.n_epochs):
-        for j in range(X.shape[0]):
-            x, y = X[j:j+1], Y[j:j+1]
-            info = f_surr(x, y)
-            loss, loss_surr, grad = info['loss'], info['surr_loss'], info['surr_grad']
-            # loss, loss_surr, grad = f_grad(x, y)
-            all_loss.append(np.sum(loss))
-            all_surr_loss.append(loss_surr)
-            # update
-            grad = param_col.flatten_values(grad)
-            # rmsprop_update(grad, optim_state)
-            optim_state.theta -= optim_state.step_size * grad
-            print np.linalg.norm(grad)
-            param_col.set_value_flat(optim_state.theta)
-        print "Epoch %d" % i_epoch
-        print "network parameters"
-        _params_val =  param_col.get_values()
-        # _ber_param = _params_val[0].T.dot(EXAMPLES_ARGS.x)
-        # if not args.no_bias: _ber_param += _params_val[1].flatten()
-        # _ber_param = sigmoid(_ber_param)
-        # print ""
-        # print "network params"
-        # pprint.pprint(_params_val)
-        # print "bernoulli param"
-        # pprint.pprint( _ber_param)
-        pprint.pprint(_params_val)
-    all_loss, all_surr_loss = np.array(all_loss), np.array(all_surr_loss)
-    plt.plot(np.convolve(all_loss, [1. / X.shape[0]] * X.shape[0], 'same'))
-    plt.plot(np.convolve(all_surr_loss, [1. / X.shape[0]] * X.shape[0], 'same'))
 
-if __name__ == "__main__":
-    main()
