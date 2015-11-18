6392e6a8610865c3c73c9d08cd908811b038d9a6
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2015-11-18 02:15:47 -0800

    isotropic gaussian

--- cgt/distributions.py
+++ cgt/distributions.py
@@ -6,15 +6,15 @@ import numpy.random as nr
 class Distribution(object):
     def lik(self, x, p):
         raise NotImplementedError
-    def loglik(self, x, p):
-        raise NotImplementedError
-    def logprob(self, x, p):
+    def loglik(self, x, *params):
+        return cgt.sum(self.logprob(x, *params))
+    def logprob(self, x, *params):
         raise NotImplementedError
     def crossent(self, p, q):
         raise NotImplementedError
     def kl(self, p, q):
         raise NotImplementedError
-    def sample(self, p):
+    def sample(self, *params):
         raise NotImplementedError
 
 # serve as a table to store past samples as an empirical distribution
@@ -25,9 +25,6 @@ class _Bernoulli(Distribution):
     """
     Bernoulli: f(k; p) = p^k (1 - p)^(1 - k), k = 0, 1
     """
-    def loglik(self, x, p):
-        """ Log likelihood of params on dataset x """
-        return cgt.sum(self.logprob(x, p))
     def logprob(self, x, p):
         """ Element-wise log prob for each component in x """
         p = core.as_node(p)
@@ -47,8 +44,6 @@ class _Bernoulli(Distribution):
 bernoulli = _Bernoulli()
 
 class _DiagonalGaussian(Distribution):
-    def loglik(self, x, mu, sigma):
-        return cgt.sum(self.logprob(x, mu, sigma))
     def logprob(self, x, mu, sigma):
         """ Calculate logprob for each row of x, mu, sigma """
         assert sigma.ndim == mu.ndim == x.ndim == 2
@@ -58,17 +53,18 @@ class _DiagonalGaussian(Distribution):
         prob_e = cgt.sum(-.5 * ((x - mu) ** 2) / sigma, axis=1, keepdims=True)
         # output shape: (size_batch, 1)
         return prob_z + prob_e
-    def sample(self, mu, sigma, shape=None, numeric=False):
-        # Sigma = np.diag(sigma)
-        # return nr.multivariate_normal(mu, Sigma)
-        raise NotImplementedError
 gaussian_diagonal = _DiagonalGaussian()
 
 class _IsotropicGaussian(Distribution):
-    def logprob(self, x, p):
-        pass
-    def sample(self, p, shape=None, numeric=False):
-        pass
+    def logprob(self, x, mu, sigma):
+        """ Calculate logprob for each row of x, mu, sigma """
+        assert sigma.ndim == mu.ndim == x.ndim == 2
+        k = x.shape[1]
+        prob_z = -.5 * (k * np.log(2. * np.pi) + k * cgt.log(sigma))
+        prob_e = cgt.sum(-.5 * cgt.broadcast('/', (x - mu) ** 2, sigma, 'xx,x1'),
+                         axis=1, keepdims=True)
+        # output shape: (size_batch, 1)
+        return prob_z + prob_e
 gaussian_isotropic = _IsotropicGaussian()
 
 class _Categorical(Distribution):
