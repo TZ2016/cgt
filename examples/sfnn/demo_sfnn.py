89f118046c79e344e98dbd6d8e962d2737a5559b
TZ2016 <tianhao.zhang.2016@gmail.com>
2015-11-26 16:22:07 -0800

    re-structure files

--- examples/sfnn/demo_sfnn.py
+++ examples/sfnn/demo_sfnn.py
@@ -2,19 +2,17 @@
 
 import os
 import cgt
-import warnings
 import traceback
 import pprint
-import matplotlib.pyplot as plt
 from cgt.core import get_surrogate_func
 from cgt import nn
 import numpy as np
 import pickle
-from sklearn.preprocessing import StandardScaler
-from scipy.special import expit as sigmoid
 from cgt.utility.param_collection import ParamCollection
 from cgt.distributions import gaussian_diagonal
 
+from opt import *
+from debug import *
 
 def err_handler(type, flag):
     print type, flag
@@ -25,54 +23,6 @@ np.seterrcall(err_handler)
 np.set_printoptions(precision=4, suppress=True)
 print cgt.get_config(True)
 cgt.check_source()
-
-
-def scale_data(X, Y, Y_var=None, scalers=None):
-    if not scalers:
-        scalers = [StandardScaler() for _ in range(2)]
-    assert len(scalers) == 2
-    s_X = scalers[0].fit_transform(X)
-    s_Y = scalers[1].fit_transform(Y)
-    if Y_var is not None:
-        scalers[1].with_mean = False
-        s_Y_var = np.square(scalers[1].transform(np.sqrt(Y_var)))
-        return s_X, s_Y, s_Y_var
-    return s_X, s_Y
-
-def generate_examples(N, x, y, p_y):
-    X = x * np.ones((N, x.size))
-    Y = y * np.ones((N, y.size))
-    for i, p in enumerate(p_y):
-        if p is not None:
-            Y[:, i] = 0.
-            Y[:, i][:int(N*p)] = 1.
-    np.random.shuffle(Y)
-    return X, Y
-
-def data_synthetic_a(N):
-    # x = y + 0.3 sin(2 * pi * y) + e, e ~ Unif(-0.1, 0.1)
-    Y = np.random.uniform(0., 1., N)
-    X = Y + 0.3 * np.sin(2. * Y * np.pi) + np.random.uniform(-.1, .1, N)
-    Z = np.random.normal(scale=.05, size=N)
-    # X = np.hstack([X, Z])
-    # Y = np.hstack([Y, Z])
-    i_s =np.argsort(X, axis=0)
-    Y, X = np.reshape(Y[i_s], (N, 1)), np.reshape(X[i_s], (N, 1))
-    Y_var = np.array([np.var(Y[i:i+20]) for i in range(N-20)] +
-                     [np.var(Y[N-20:])] * 20).reshape((N, 1))
-    return X, Y, Y_var
-
-def data_sigm_multi(N, p):
-    if not isinstance(p, (list, tuple)):
-        assert isinstance(p, int)
-        p = [1. / p] * p
-    X = np.random.uniform(-10., 10., N)
-    Y = sigmoid(X)
-    Y += np.random.normal(0., .1, N)
-    y = np.random.multinomial(1, p, size=N)
-    Y += np.nonzero(y)[1]
-    Y, X = Y.reshape((N, 1)), X.reshape((N, 1))
-    return X, Y
 
 
 def hybrid_layer(X, size_in, size_out, size_random, dbg_out=[]):
@@ -162,45 +112,6 @@ def make_funcs(net_in, net_out, config, dbg_out=None):
     # TODO_TZ f_step seems not to fail if X has wrong dim
     return params, f_step, None, None, f_surr
 
-def rmsprop_create(theta, step_size, decay_rate=.95, eps=1.e-6):
-    optim_state = dict(theta=theta, step_size=step_size,
-                   decay_rate=decay_rate,
-                   sqgrad=np.zeros_like(theta) + eps,
-                   scratch=np.empty_like(theta), count=0, type='rmsprop')
-    return optim_state
-
-def rmsprop_update(grad, state):
-    state['sqgrad'][:] *= state['decay_rate']
-    state['count'] *= state['decay_rate']
-    np.square(grad, out=state['scratch']) # scratch=g^2
-    state['sqgrad'] += state['scratch']
-    state['count'] += 1
-    np.sqrt(state['sqgrad'], out=state['scratch']) # scratch = sum of squares
-    np.divide(state['scratch'], np.sqrt(state['count']), out=state['scratch']) # scratch = rms
-    np.divide(grad, state['scratch'], out=state['scratch']) # scratch = grad/rms
-    np.multiply(state['scratch'], state['step_size'], out=state['scratch'])
-    state['theta'][:] += state['scratch']
-
-def adam_create(theta, step_size=1.e-3, beta1=.9, beta2=.999, eps=1.e-8):
-    optim_state = dict(theta=theta, step_size=step_size,
-                       beta1=beta1, beta2=beta2, eps=eps, _t=0,
-                       _m=np.zeros_like(theta), _v=np.zeros_like(theta),
-                       scratch=np.zeros_like(theta), type='adam')
-    return optim_state
-
-def adam_update(grad, state):
-    state['_t'] += 1
-    state['_m'] *= state['beta1']
-    state['_m'] += (1 - state['beta1']) * grad
-    state['_v'] *= state['beta2']
-    np.square(grad, out=state['scratch'])
-    state['_v'] += (1 - state['beta2']) * state['scratch']
-    np.sqrt(state['_v'], out=state['scratch'])
-    np.divide(state['_m'], state['scratch'] + state['eps'], out=state['scratch'])
-    state['scratch'] *= state['step_size'] * \
-                        np.sqrt(1. - state['beta2'] ** state['_t']) / \
-                        (1. - state['beta1'] ** state['_t'])
-    state['theta'] += state['scratch']
 
 def step(X, Y, workspace, config, Y_var=None, dbg_iter=None, dbg_done=None):
     if config['debug'] and (dbg_iter is None or dbg_done is None):
@@ -284,125 +195,10 @@ def create(args):
     return workspace
 
 
-def example_debug(args, X, Y, Y_var=None):
-    def safe_path(rel_path):
-        abs_path = os.path.join(out_path, rel_path)
-        d = os.path.dirname(abs_path)
-        if not os.path.exists(d):
-            warnings.warn("Making new directory: %s" % d)
-            os.makedirs(d)
-        return abs_path
-    def h_ax(ax, title=None, x=None, y=None):
-        if not isinstance(ax, (tuple, list, np.ndarray)): ax = [ax]
-        for a in ax:
-            if title: a.set_title(title)
-            if x:
-                if x == 'hide':
-                    plt.setp(a.get_xticklabels(), visible=False)
-                elif x == 'mm':  # min/max
-                    plt.setp(a.get_xticklabels()[1:-1], visible=False)
-                    plt.setp(a.get_xticklabels()[0], visible=True)
-                    plt.setp(a.get_xticklabels()[-1], visible=True)
-                else:
-                    raise KeyError
-            if y:
-                if y == 'hide':
-                    plt.setp(a.get_yticklabels(), visible=False)
-                elif y == 'mm':  # min/max
-                    plt.setp(a.get_yticklabels()[1:-1], visible=False)
-                    plt.setp(a.get_yticklabels()[0], visible=True)
-                    plt.setp(a.get_yticklabels()[-1], visible=True)
-                else:
-                    raise KeyError
-    N, _ = X.shape
-    out_path = args['dump_path']
-    conv_smoother = lambda x: np.convolve(x, [1. / N] * N, mode='valid')
-    _ix, _iy = args['dbg_plot_samples']['x_dim'], args['dbg_plot_samples']['y_dim']
-    # cache
-    ep_net_distr = []
-    it_loss_surr = []
-    it_theta_comp = []
-    it_grad_norm, it_grad_norm_comp = [], []
-    it_theta_norm, it_theta_norm_comp = [], []
-    def dbg_iter(num_epochs, num_iters, info, workspace):
-        optim_state = workspace['optim_state']
-        param_col = workspace['param_col']
-        f_step = workspace['f_step']
-        it_loss_surr.append(info['objective'])
-        it_grad_norm.append(np.linalg.norm(optim_state['scratch']))
-        it_grad_norm_comp.append([np.linalg.norm(g) / np.size(g)
-                                  for g in info['grad']])
-        it_theta_norm.append(np.linalg.norm(optim_state['theta']))
-        it_theta_norm_comp.append([np.linalg.norm(t) / np.size(t)
-                                   for t in param_col.get_values()])
-        it_theta_comp.append(np.copy(optim_state['theta']))
-        if num_iters == 0:  # new epoch
-            print "Epoch %d" % num_epochs
-            print "Gradient norm = %f" % it_grad_norm[-1]
-            print "Theta norm = %f" % it_theta_norm[-1]
-            if args['dbg_plot_samples']['plot']:
-                s_ind = np.random.choice(N, size=args['dbg_plot_samples']['batch'], replace=False)
-                s_X = X[s_ind, :]
-                if args['variance'] == 'in':
-                    s_Y_var_in = Y_var[s_ind, :]
-                    s_Y_mean, s_Y_var = f_step(s_X, s_Y_var_in)
-                else:
-                    s_Y_mean, s_Y_var = f_step(s_X)
-                err_plt = lambda: plt.errorbar(s_X[:, _ix], s_Y_mean[:, _iy],
-                                               yerr=np.sqrt(s_Y_var[:, _iy]), fmt='none')
-                ep_net_distr.append((num_epochs, err_plt))
-    def dbg_done(workspace):
-        kw_ticks = {
-            'xticks': np.arange(args['n_epochs']) * N,
-            'xticklabels': np.arange(args['n_epochs']).astype(str)
-        }
-        param_col = workspace['param_col']
-        optim_state = workspace['optim_state']
-        # save params
-        print "Saving params to %s" % safe_path('.')
-        pickle.dump(args, open(safe_path('args.pkl'), 'w'))
-        pickle.dump(param_col.get_values(), open(safe_path('params.pkl'), 'w'))
-        pickle.dump(optim_state, open(safe_path('__snapshot.pkl'), 'w'))
-        pickle.dump(np.array(it_theta_comp), open(safe_path('params_history.pkl'), 'w'))
-        # plot overview
-        f, axs = plt.subplots(3, 1, sharex=True, subplot_kw=kw_ticks)
-        f.suptitle('overview')
-        axs[0].plot(conv_smoother(it_loss_surr))
-        h_ax(axs[0], title='loss', x='hide')
-        axs[1].plot(conv_smoother(it_grad_norm)); axs[1].set_title('grad')
-        h_ax(axs[1], title='grad', x='hide')
-        axs[2].plot(conv_smoother(it_theta_norm)); axs[2].set_title('theta')
-        h_ax(axs[2], title='theta')
-        f.savefig(safe_path('overview.png')); plt.close(f)
-        # plot grad norm component-wise
-        _grad_norm_cmp = np.array(it_grad_norm_comp).T
-        f, axs = plt.subplots(_grad_norm_cmp.shape[0], 1, sharex=True, subplot_kw=kw_ticks)
-        f.suptitle('grad norm layer-wise')
-        for _i, _ax in enumerate(axs):
-            _ax.plot(conv_smoother(_grad_norm_cmp[_i]))
-        h_ax(axs[:-1], x='hide'); h_ax(axs, y='mm')
-        f.tight_layout(); f.savefig(safe_path('norm_grad_cmp.png')); plt.close(f)
-        # plot theta norm component-wise
-        _theta_norm_cmp = np.array(it_theta_norm_comp).T
-        f, axs = plt.subplots(_theta_norm_cmp.shape[0], 1, sharex=True, subplot_kw=kw_ticks)
-        f.suptitle('theta norm layer-wise')
-        for _i, _ax in enumerate(axs):
-            _ax.plot(conv_smoother(_theta_norm_cmp[_i]))
-        h_ax(axs[:-1], x='hide'); h_ax(axs, y='mm')
-        f.tight_layout(); f.savefig(safe_path('norm_theta_cmp.png')); plt.close(f)
-        # plot samples for each epoch
-        if args['dbg_plot_samples']['plot']:
-            for _e, _distr in enumerate(ep_net_distr):
-                _ttl = 'epoch_%d.png' % _e
-                plt.scatter(X[:, _ix], Y[:, _iy], alpha=0.5, color='y', marker='*')
-                plt.gca().set_autoscale_on(False)
-                _distr[1](); plt.title(_ttl)
-                plt.savefig(safe_path('_sample/' + _ttl)); plt.cla()
-    return dbg_iter, dbg_done
-
 if __name__ == "__main__":
     import yaml
     import time
+    from data import scale_data, data_synthetic_a
 
     DUMP_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '_tmp')
     PARAMS_PATH = os.path.join(DUMP_ROOT, '../sfnn_params.yaml')
